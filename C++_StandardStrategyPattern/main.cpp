9115deb51fc92f56f6df737e001e603f400bae83
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-10-23 16:22:50 -0400

    Create GameCharacter class hierarchy and HealthCalcClass class hierarchy w calc fns.

--- C++_StandardStrategyPattern/main.cpp
+++ C++_StandardStrategyPattern/main.cpp
@@ -7,9 +7,53 @@
 //
 
 #include <iostream>
+using namespace std;
+
+
+class GameCharacter;
+
+class HealthCalcClass {
+public:
+    virtual int calc(const GameCharacter&) const {
+        return 0;
+    }
+};
+
+class SlowHealthDegr : public HealthCalcClass {
+public:
+    int calc(const GameCharacter&) const {
+        return 5;
+    };
+};
+
+HealthCalcClass defaultHealthCalc;
+
+class GameCharacter {
+public:
+    explicit GameCharacter(HealthCalcClass *phcf = &defaultHealthCalc) : pHealthCalc(phcf) {};
+    int healthValue() const {
+        return pHealthCalc->calc(*this);
+    }
+    void setHealthCalcClass(HealthCalcClass& hcc) {
+        pHealthCalc = &hcc;
+    }
+private:
+    HealthCalcClass *pHealthCalc;
+};
+
+class BadEvilGuy : public GameCharacter {
+public:
+    explicit BadEvilGuy(HealthCalcClass * phcf) : GameCharacter(phcf) {};
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    SlowHealthDegr slow;
+    BadEvilGuy ebg1(&slow);
+    cout << ebg1.healthValue() << endl;
+    HealthCalcClass defHealth;
+    BadEvilGuy ebg2(&defHealth);
+    cout << ebg2.healthValue() << endl;
+    
+    
     return 0;
 }
